16f0cf932b533aaf3cfa67fbb8b33412050860bb
Özge Kocaoğlu <ozge.kocaoglu@outlook.com>
2021-09-03 23:56:56 +0300

    Enviroment and player movement added.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float forwardSpeed;
+    [SerializeField] private float swerveSpeed;
+    [SerializeField] private DynamicJoystick joystick;
+
+    private void Update()
+    {
+        ForwardMove();
+        SwerveMove();
+    }
+
+    private void ForwardMove()
+    {
+        transform.Translate(transform.forward * Time.deltaTime * forwardSpeed);
+    }
+
+    private void SwerveMove()
+    {
+        float clampedHorizontal = Mathf.Clamp(transform.position.x + joystick.Horizontal * swerveSpeed * Time.deltaTime, -3.45F, 3.45F);
+        Vector3 direction = new Vector3(clampedHorizontal, transform.position.y, transform.position.z);
+        transform.position = Vector3.MoveTowards(transform.position, direction, 1);
+    }
+}
